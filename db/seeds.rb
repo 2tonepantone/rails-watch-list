4a79d871cc903f33f179def47141db5c2236740b
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-13 11:59:06 +0900

    add img upload and change configs to cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,10 @@ Movie.create(title: "Ocean's Eight",
              rating: 7.0)
 puts 'Finished adding movies to your DB!!!'
 
-puts 'Adding lists to your DB...'
-List.create(name: 'Action')
-List.create(name: 'Comedy')
-List.create(name: 'Drama')
-List.create(name: 'Documentary')
-List.create(name: 'Horror')
-puts 'Finished adding list to your DB!!!'
+# puts 'Adding lists to your DB...'
+# List.create(name: 'Action')
+# List.create(name: 'Comedy')
+# List.create(name: 'Drama')
+# List.create(name: 'Documentary')
+# List.create(name: 'Horror')
+# puts 'Finished adding list to your DB!!!'
